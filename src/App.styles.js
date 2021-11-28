2b49e0b7f75c80ef3ac9fb66f3bc0c759e55d7ce
Aurelio Gonzalez <agg156231@gmail.com>
2021-11-27 20:07:15 -0700

    Change the background for #424242

--- src/App.styles.js
+++ src/App.styles.js
@@ -6,7 +6,7 @@ const AppContainer = styled.div`
     align-items: center;
     width: 100%;
     height: 100vh;
-    background-image: radial-gradient(circle at 111.24% 14.64%, #fbf3e6 0, #f9eddd 10%, #f7e5d3 20%, #f5ddc9 30%, #f4d3bf 40%, #f2c9b5 50%, #f0beac 60%, #efb5a6 70%, #edaba1 80%, #eca39f 90%, #e99b9f 100%);  
+    background-color: #424242;
 `
-
+/* radial-gradient(circle at 111.24% 14.64%, #fbf3e6 0, #f9eddd 10%, #f7e5d3 20%, #f5ddc9 30%, #f4d3bf 40%, #f2c9b5 50%, #f0beac 60%, #efb5a6 70%, #edaba1 80%, #eca39f 90%, #e99b9f 100%); */
 export { AppContainer }
